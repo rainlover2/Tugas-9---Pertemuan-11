dfb7d46593905af91cf8895ba2137a468356de27
rainlover2 <naucalista@gmail.com>
2025-11-23 23:11:49 +0700

    Tugas 8

---
+++ android/app/src/main/kotlin/com/example/toko_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.toko_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/service/api.dart
@@ -0,0 +1,6 @@
+class Api {
+  static const String baseUrl = "http://10.0.2.2:8080";
+  static String login = "$baseUrl/login";
+  static String register = "$baseUrl/register";
+  static String produk = "$baseUrl/produk";
+}
